6bde7333560f4bc1f4a5f33e1e5d6779a9f73024
chalimbu <sebitaszapata12@gmail.com>
2022-07-04 09:47:56 -0500

    enunciado ejercisio 1

---
+++ src/co/com/twodotone.java
@@ -0,0 +1,9 @@
+package co.com;
+
+public class twodotone {
+    //2.1 pag 94 cracking the coding interview.
+    // Remove dups Write code to remove duplicates from an unsorted linked list.
+    //Follow up.
+    //How would you solve this problem if a temporary buffer is not allowed.
+
+}
